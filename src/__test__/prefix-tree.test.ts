0a12f470b083685f3b8fadca179cdfb893031ac1
Mrtza-javidi <morteza.javidi.webdev@gmail.comi>
2024-09-20 17:54:02 +0330

    test: validate priority-based results in test cases

--- src/__test__/prefix-tree.test.ts
+++ src/__test__/prefix-tree.test.ts
@@ -1,4 +1,4 @@
-import { autocomplete } from "../index";
+import { autocomplete, addPhrase } from "../index";
 
 describe("Autocomplete", () => {
   const words = [
@@ -16,15 +16,53 @@ describe("Autocomplete", () => {
   ];
   it('should return the first word or phrase for prefix "mo"', () => {
     const result = autocomplete("mo", words);
-    result && expect(result[0]).toBe("morteza");
+    expect(result![0]).toBe("morteza");
   });
 
   it('should return the first word or phrase for prefix "h"', () => {
     const result = autocomplete("h", words);
-    result && expect(result[0]).toBe("haniyeh");
+    expect(result![0]).toBe("haniyeh");
   });
+
   it("should return null if no word starts with the given prefix", () => {
     const result = autocomplete("z", words);
+    expect(result).toBeNull();
+  });
+
+  it("should return the phrase with the highest priority for a given prefix", () => {
+    addPhrase("morteza", 2);
+    addPhrase("mohammad", 1.2);
+    addPhrase("morid", 2);
+
+    const result = autocomplete("mo");
+    expect(result![0]).toEqual("mohammad");
+  });
+
+  it("should handle phrases with the same prefix but different priorities", () => {
+    addPhrase("mohammad", 1.2);
+    addPhrase("morteza", 1);
+    addPhrase("morid", 2);
+
+    const result = autocomplete("mo");
+    expect(result![0]).toEqual("morteza");
+  });
+
+  it("should handle both phrases with and without priority", () => {
+    addPhrase("morteza", 1);
+    addPhrase("mohammad", 1.2);
+    addPhrase("morid", 2);
+    addPhrase("hossein");
+    addPhrase("haniyeh");
+
+    const result = autocomplete("mo");
+    expect(result![0]).toEqual("morteza");
+  });
+
+  it("should return null if no word starts with the given prefix", () => {
+    addPhrase("haniyeh");
+    addPhrase("sadegh");
+
+    const result = autocomplete("z");
     expect(result).toBeNull();
   });
 });
