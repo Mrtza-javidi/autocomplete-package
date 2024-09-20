4786c3cb697dc83381f9e41565ab01fe6f421bf3
Mrtza-javidi <morteza.javidi.webdev@gmail.comi>
2024-09-20 17:53:37 +0330

    feat(autocomplete): update to return highest priority phrase

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { Trie } from "./prefix-tree/trie";
 
+const trie = new Trie();
+
+export const addPhrase = (word: string, priority?: number): void => {
+  trie.addPhrase(word, priority);
+};
+
 export const autocomplete = (
   prefix: string,
-  words: string[]
+  words?: string[]
 ): string[] | null => {
-  const trie = new Trie();
-  words.forEach((word) => trie.insert(word));
-  return trie.suggest(prefix);
+  if (words) words.forEach((word) => trie.addPhrase(word)); // Add words to the Trie
+  const suggestions = trie.suggest(prefix);
+  return suggestions && suggestions.length > 0 ? suggestions : null;
 };
