a4c4ac975fb65fff9a0024e015e3b779e9473ee8
Mrtza-javidi <morteza.javidi.webdev@gmail.comi>
2024-09-20 17:53:09 +0330

    feat(trie): implement addPhrase with optional priority support

--- src/prefix-tree/trie.ts
+++ src/prefix-tree/trie.ts
@@ -1,12 +1,23 @@
 class TrieNode {
-  children: { [key: string]: TrieNode } = {};
-  isEndOfWord: boolean = false;
+  children: { [key: string]: TrieNode };
+  isEndOfWord: boolean;
+  priority: number | null;
+
+  constructor() {
+    this.children = {};
+    this.isEndOfWord = false;
+    this.priority = null;
+  }
 }
 
 export class Trie {
-  private root: TrieNode = new TrieNode();
+  private root: TrieNode;
 
-  insert(word: string) {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  addPhrase(word: string, priority: number | null = null): void {
     let node = this.root;
     for (const char of word) {
       if (!node.children[char]) {
@@ -15,6 +26,7 @@ export class Trie {
       node = node.children[char];
     }
     node.isEndOfWord = true;
+    node.priority = priority !== null ? priority : 1000000;
   }
 
   suggest(prefix: string): string[] | null {
@@ -23,18 +35,21 @@ export class Trie {
       if (!node.children[char]) return null;
       node = node.children[char];
     }
-    const suggestions = this.collectWords(node, prefix);
-    return suggestions.length > 0 ? suggestions : null;
+    const suggestions: { word: string; priority: number }[] = [];
+    this.collectSuggestions(node, prefix, suggestions);
+    suggestions.sort((a, b) => a.priority - b.priority);
+    return suggestions.map((suggestion) => suggestion.word);
   }
 
-  private collectWords(node: TrieNode, prefix: string): string[] {
-    let results: string[] = [];
-    if (node.isEndOfWord) results.push(prefix);
-    for (const char in node.children)
-      results = results.concat(
-        this.collectWords(node.children[char], prefix + char)
-      );
+  private collectSuggestions(
+    node: TrieNode,
+    prefix: string,
+    suggestions: { word: string; priority: number }[]
+  ): void {
+    if (node.isEndOfWord)
+      suggestions.push({ word: prefix, priority: node.priority! });
 
-    return results;
+    for (const char in node.children)
+      this.collectSuggestions(node.children[char], prefix + char, suggestions);
   }
 }
