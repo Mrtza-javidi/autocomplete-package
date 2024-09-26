a82adb48448cdea6f125943480951c9607dc9aaa
Mrtza-javidi <morteza.javidi.webdev@gmail.comi>
2024-09-26 14:21:23 +0330

    feat(trie): enable chaining of `addPhrase` and `suggest` methods to `autocomplete`

--- src/prefix-tree/trie.ts
+++ src/prefix-tree/trie.ts
@@ -1,14 +1,4 @@
-class TrieNode {
-  children: { [key: string]: TrieNode };
-  isEndOfWord: boolean;
-  priority: number | null;
-
-  constructor() {
-    this.children = {};
-    this.isEndOfWord = false;
-    this.priority = null;
-  }
-}
+import TrieNode from "./trie-node";
 
 export class Trie {
   private root: TrieNode;
@@ -17,24 +7,29 @@ export class Trie {
     this.root = new TrieNode();
   }
 
-  addPhrase(word: string, priority: number | null = null): void {
+  addPhrase(word: string, priority: number | null = null): Trie {
     let node = this.root;
-    for (const char of word) {
+    word.split("").forEach((char) => {
       if (!node.children[char]) {
         node.children[char] = new TrieNode();
       }
       node = node.children[char];
-    }
+    });
+
     node.isEndOfWord = true;
-    node.priority = priority !== null ? priority : 1000000;
+    node.priority = priority !== null ? priority : Infinity;
+    return this;
   }
 
   suggest(prefix: string): string[] | null {
     let node = this.root;
-    for (const char of prefix) {
-      if (!node.children[char]) return null;
+    const found = prefix.split("").every((char) => {
+      if (!node.children[char]) return false;
       node = node.children[char];
-    }
+      return true;
+    });
+    if (!found) return null;
+
     const suggestions: { word: string; priority: number }[] = [];
     this.collectSuggestions(node, prefix, suggestions);
     suggestions.sort((a, b) => a.priority - b.priority);
@@ -49,7 +44,8 @@ export class Trie {
     if (node.isEndOfWord)
       suggestions.push({ word: prefix, priority: node.priority! });
 
-    for (const char in node.children)
+    Object.keys(node.children).forEach((char) => {
       this.collectSuggestions(node.children[char], prefix + char, suggestions);
+    });
   }
 }
