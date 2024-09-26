ebde5bf6d040bf072fc3f69c058a2aca2f9c1f1e
Mrtza-javidi <morteza.javidi.webdev@gmail.comi>
2024-09-26 21:03:50 +0330

    test: update tests for revised autocomplete implementation

--- src/__test__/prefix-tree.test.ts
+++ src/__test__/prefix-tree.test.ts
@@ -1,8 +1,10 @@
-import autocomplete from "../index";
+import { autocomplete } from "../index";
+
+const testAutocomplete = autocomplete();
 
 describe("Autocomplete", () => {
   beforeEach(() => {
-    autocomplete
+    testAutocomplete
       .addPhrase("morteza", 1.5)
       .addPhrase("mohammad", 1.4)
       .addPhrase("moghimi", 3)
@@ -20,27 +22,27 @@ describe("Autocomplete", () => {
   });
 
   it("should return the phrase with the highest priority for a given prefix", () => {
-    const result = autocomplete.suggest("mo");
+    const result = testAutocomplete.suggest("mo");
     expect(result![0]).toEqual("mohammad");
   });
 
   it("should handle both phrases with and without priority", () => {
-    const result = autocomplete.suggest("ha");
+    const result = testAutocomplete.suggest("ha");
     expect(result![0]).toEqual("haleh");
   });
 
   it("should return the first phrase between a couple of them without any priority", () => {
-    const result = autocomplete.suggest("l");
+    const result = testAutocomplete.suggest("l");
     expect(result![0]).toEqual("laleh");
   });
 
   it("should return the first phrase between a couple of them with same priorities", () => {
-    const result = autocomplete.suggest("fa");
+    const result = testAutocomplete.suggest("fa");
     expect(result![0]).toEqual("farhad");
   });
 
   it("should return null if no word starts with the given prefix", () => {
-    const result = autocomplete.suggest("z");
+    const result = testAutocomplete.suggest("z");
     expect(result).toBeNull();
   });
 });
