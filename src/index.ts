b11ab4e4dacd400aafd3826b7d9088d65fb68d1f
Mrtza-javidi <morteza.javidi.webdev@gmail.comi>
2024-09-26 21:03:18 +0330

    refactor(autocomplete): instantiate new trie for each usage

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,3 @@
 import { Trie } from "./prefix-tree/trie";
 
-const trie = new Trie();
-
-interface Autocomplete {
-  addPhrase(word: string, priority?: number): Autocomplete; // Return type enables chaining
-  suggest(prefix: string): string[] | null;
-}
-
-const autocomplete: Autocomplete = {
-  addPhrase(word: string, priority?: number): typeof autocomplete {
-    trie.addPhrase(word, priority);
-    return this;
-  },
-
-  suggest(prefix: string): string[] | null {
-    return trie.suggest(prefix);
-  },
-};
-
-export default autocomplete;
+export const autocomplete = () => new Trie();
